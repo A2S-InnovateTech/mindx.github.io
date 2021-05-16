34a07884355442ecff0c10b1cf5258c8d0e5c401
Nidhirajput1301 <nidhirajput1301@gmail.com>
2021-05-17 01:18:10 +0530

    razorpay integration

--- src/components/popup2/index.js
+++ src/components/popup2/index.js
@@ -1,7 +1,7 @@
 import React, {useState, useEffect }  from 'react';
 import { Modal} from 'react-bootstrap';
 import './popup2.css';
-import axios from "axios";
+
 
 const Popup2 = () =>  {
     const [show, setShow] = useState(true);
@@ -11,90 +11,18 @@ const Popup2 = () =>  {
       window.location.assign("/login")
     };
   
-
-    // function loadScript(src) {
-    //     return new Promise((resolve) => {
-    //         const script = document.createElement("script");
-    //         script.src = src;
-    //         script.onload = () => {
-    //             resolve(true);
-    //         };
-    //         script.onerror = () => {
-    //             resolve(false);
-    //         };
-    //         document.body.appendChild(script);
-    //     });
-    // }
-
-    // async function displayRazorpay() {
-    //     const res = await loadScript(
-    //         "https://checkout.razorpay.com/v1/checkout.js"
-    //     );
-
-    //     if (!res) {
-    //         alert("Razorpay SDK failed to load. Are you online?");
-    //         return;
-    //     }
-
-    //     const result = await axios.post("http://localhost:3000/payment/orders");
-
-    //     if (!result) {
-    //         alert("Server error. Are you online?");
-    //         return;
-    //     }
-
-    //     const { amount, id: order_id, currency } = result.data;
-        
-    //     const options = {
-    //         key: "rzp_test_r6FiJfddJh76SI", // Enter the Key ID generated from the Dashboard
-    //         amount: amount.toString(),
-    //         currency: currency,
-    //         name: "Soumya Corp.",
-    //         description: "Test Transaction",
-    //         image: { logo },
-    //         order_id: order_id,
-    //         handler: async function (response) {
-    //             const data = {
-    //                 orderCreationId: order_id,
-    //                 razorpayPaymentId: response.razorpay_payment_id,
-    //                 razorpayOrderId: response.razorpay_order_id,
-    //                 razorpaySignature: response.razorpay_signature,
-    //             };
-
-    //             const result = await axios.post("http://localhost:3000/payment/success", data);
-
-    //             alert(result.data.msg);
-    //         },
-    //          prefill: {
-    //             name: "Soumya Dey",
-    //             email: "SoumyaDey@example.com",
-    //              contact: "9999999999",
-    //          },
-    //          notes: {
-    //              address: "Soumya Dey Corporate Office",
-    //          },
-    //          theme: {
-    //              color: "#61dafb",
-    //          },
-    //     };
-
-    //     const paymentObject = new window.Razorpay(options);
-    //     paymentObject.open();
-    // }
-
-
-    
     const options = {
-        key: 'YOUR_KEY',
-        amount: '100', 
-        name: 'Acme shop',
+        key: 'YOUR__KEY',
+        amount: '10000',
+        currency: 'INR', 
+        name: 'Student',
         description: 'some description',
         image: 'https://cdn.razorpay.com/logos/7K3b6d18wHwKzL_medium.png',
         handler: function(response) {
             alert(response.razorpay_payment_id);
         },
         prefill: {
-            name: 'Gaurav',
+            name: 'demo',
             contact: '9999999999',
             email: 'demo@demo.com'
         },
@@ -102,7 +30,7 @@ const Popup2 = () =>  {
             address: 'some address'
         },
         theme: {
-            color: 'blue',
+            color: '#10222E',
             hide_topbar: false
         }
     };
@@ -117,6 +45,7 @@ const Popup2 = () =>  {
         script.async = true;
         document.body.appendChild(script);
     }, []);
+
 
   
     return (
@@ -141,7 +70,6 @@ const Popup2 = () =>  {
             <p  className="subscribe_p">
              To enjoy our services hassle free you have to subscribe it now :
             </p>
-            {/* onClick={displayRazorpay} */}
             <div className="subscribe_btn" onClick={openPayModal}>
               <p className="button-text">Subscribe</p>
             </div>
